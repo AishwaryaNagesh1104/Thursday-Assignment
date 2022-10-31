dddef8438d2ce63c906c64a9e3eb84add3af69bd
AishwaryaNagesh1104 <117056038+AishwaryaNagesh1104@users.noreply.github.com>
2022-10-31 09:40:59 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,15 @@
+package samp1;
+
+public class sample {
+	
+	public static void main(String[] args) {
+		int a=10,b=6,c=7;
+		b=a++;
+		c=++a;
+		System.out.println("a:"+a+"b:"+b);
+		System.out.println("a:"+a+"c:"+c);
+
+	}
+
+}
+
