cf438f0848f9668c6082cd1caeefcd2135b9e73e
AishwaryaNagesh1104 <117056038+AishwaryaNagesh1104@users.noreply.github.com>
2022-11-10 13:15:12 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,17 @@
+package studentdetails;
+
+public class StudentDetails {
+		
+		    public static void main(String arg[])
+		    {
+		        String names[] = { "Rajesh", "Suresh", "Ramesh", "Kamlesh", "Vignesh" };
+		        int marks[] = { 45, 78, 83, 77, 93 };
+		        char sections[] = { 'A', 'B', 'A', 'A', 'B' };
+		        
+		        for(int i = 0; i < names.length; i++)
+		        {
+		            System.out.println( names[i] + " in section " + sections[i] + " got " + marks[i] + " marks." );
+		        }    
+		    }
+		}
+
